3db733ac3e55c4faebbf43fb0f818fba62a460d6
Aria Zanganeh <ariazanganeh@levo.com.au>
2017-05-17 06:26:15 +1000

    Add support multi level entity type

--- src/utility/typeHelper.ts
+++ src/utility/typeHelper.ts
@@ -6,4 +6,18 @@ export class TypeHelper
         var results = (funcNameRegex).exec((<any> inputClass).constructor.toString());
         return (results && results.length > 1) ? results[1] : "";
     }
+
+    static getParentTypeNames(inputClas) : Array<string>
+    {
+        let classNames = [];
+        let obj = Object.getPrototypeOf(inputClas);
+        let className: string;
+
+        while ((className = TypeHelper.getTypeName(obj)) !== "Object") {
+            classNames.push(className);
+            obj = Object.getPrototypeOf(obj);
+        }        
+
+        return classNames;
+    }
 }
