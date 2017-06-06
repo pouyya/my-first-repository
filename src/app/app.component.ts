a5283be55dd6fffc916ffc6981b8378de76eee8d
rahil051 <infernus.rk31@gmail.com>
2017-06-06 16:59:28 +0500

    SC-44 1. Separated out app routes and created routes for sales 2. Written structure to navigate between different Menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Stores } from './../pages/stores/stores';
-import { Employees } from './../pages/employees/employees';
-import { Services } from './../pages/service/service';
+import { AppMenu } from './menu/app.menu';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
-import { Products } from '../pages/products/products';
-import { Sales } from '../pages/sales/sales';
-import { Settings } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
@@ -20,17 +15,7 @@ export class ShortCutsApp {
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', icon: 'home', component: HomePage },
-      { title: 'POS', icon: 'cash', component: Sales },
-      { title: 'Products', icon: 'pricetags', component: Products },
-      { title: 'Service', icon: 'bowtie', component: Services },
-      { title: 'Employees', icon: 'contacts', component: Employees },
-      { title: 'Stores', icon: 'basket', component: Stores },
-      { title: 'Settings', icon: 'cog', component: Settings },
-    ];
+    this.pages = AppMenu.routes;
   }
 
   initializeApp() {
@@ -41,6 +26,8 @@ export class ShortCutsApp {
   }
 
   openPage(page) {
+    page.menu !== null && ( this.pages = page.menu.routes );
     this.nav.setRoot(page.component);
+
   }
 }
