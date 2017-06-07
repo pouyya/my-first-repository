8470d627841215db06bab6550b873b7d611c984a
rahil051 <infernus.rk31@gmail.com>
2017-06-07 16:03:14 +0500

    1. Relocated app and sales menu location

---
+++ src/menus/app.menu.ts
@@ -0,0 +1,23 @@
+import { SalesMenu } from './sales.menu';
+import { Settings } from './../pages/settings/settings';
+import { Stores } from './../pages/stores/stores';
+import { Employees } from './../pages/employees/employees';
+import { Services } from './../pages/service/service';
+import { Products } from './../pages/products/products';
+import { Sales } from './../pages/sales/sales';
+import { HomePage } from './../pages/home/home';
+
+
+export class AppMenu {
+
+  static routes: Array<any> = [
+    { title: 'Home', icon: 'home', component: HomePage, menu: null },
+    { title: 'POS', icon: 'cash', component: Sales, menu: { routes: SalesMenu.routes } },
+    { title: 'Products', icon: 'pricetags', component: Products, menu: null },
+    { title: 'Service', icon: 'bowtie', component: Services, menu: null },
+    { title: 'Employees', icon: 'contacts', component: Employees, menu: null },
+    { title: 'Stores', icon: 'basket', component: Stores, menu: null },
+    { title: 'Settings', icon: 'cog', component: Settings, menu: null }
+  ];
+
+}
