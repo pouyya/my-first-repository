4bd0d2db07b47279ec51e8e617d50d45bdeddaa7
Aria Zanganeh <ariazanganeh@levo.com.au>
2017-08-08 08:00:05 +1000

    SC-72 Resolve issue related to not loading user setting while loading the app

--- src/pages/deploy/deploy.ts
+++ src/pages/deploy/deploy.ts
@@ -1,3 +1,6 @@
+import { UserService } from './../../services/userService';
+import { AppSettings } from './../../model/appSettings';
+import { AppSettingsService } from './../../services/appSettingsService';
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
@@ -11,35 +14,49 @@ export class DeployPage {
 
 	public updateText: String = '';
 
-	constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public deploy: Deploy) {
+	constructor(
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public platform: Platform,
+		public deploy: Deploy,
+		private appSettingsService: AppSettingsService,
+		private userService: UserService) {
 
 		if (platform.is('core')) {
-			navCtrl.setRoot(HomePage);
-			return;
+			this.loadUserInfoAndNavigateToHome();
 		}
+		else {
+			this.deploy.channel = 'dev';
+			this.deploy.check().then((snapshotAvailable: boolean) => {
+				if (snapshotAvailable) {
+					this.updateText = 'Grabbing the best experience for you';
+					console.log('There\'s an update!');
+					this.deploy.download().then(() => {
+						this.updateText = 'Updating...';
 
-		this.deploy.channel = 'dev';
-		this.deploy.check().then((snapshotAvailable: boolean) => {
-			if (snapshotAvailable) {
-				this.updateText = 'Grabbing the best experience for you';
-				console.log('There\'s an update!');
-				this.deploy.download().then(() => {
-					this.updateText = 'Updating...';
-
-					return this.deploy.extract();
-				}).then(() => {
-					this.deploy.load();
-				});
-			} else {
-				console.log('No new code :(');
-
-				navCtrl.setRoot(HomePage);
-			}
-		});
+						return this.deploy.extract();
+					}).then(() => {
+						this.deploy.load();
+					});
+				} else {
+					console.log('No new code :(');
+					this.loadUserInfoAndNavigateToHome();
+				}
+			});
+		}
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad DeployPage');
 	}
 
+	loadUserInfoAndNavigateToHome() {
+		this.appSettingsService.get().then((setting: AppSettings) => {
+			let user = this.userService.getLoggedInUser();
+			user.settings.defaultTax = setting.defaultTax;
+			user.settings.taxType = setting.taxType;
+
+			this.navCtrl.setRoot(HomePage);
+		});
+	}
 }
