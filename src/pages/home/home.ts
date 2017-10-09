0aa95c2b0379e58d68d7286f1056fbff8e7d0fbe
Aria Zanganeh <ariazanganeh@levo.com.au>
2017-10-09 21:08:10 +1100

    SC-66 Add HomePage module

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
+import { BackOfficeModule } from './../../modules/backOfficeModule';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PageModule } from '../../metadata/pageModule';
 
+@PageModule(() => BackOfficeModule)
 @Component({
   selector: 'page-variables',
   templateUrl: 'home.html'
