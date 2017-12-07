aa3310c65276080d6f3e6d221dd523ea79e804c1
rahil051 <infernus.rk31@gmail.com>
2017-12-07 17:02:34 +0500

    SC-94 1. Added Logout component which clears ionic storage and localstorage 2. Added Logout to BackOffice

--- src/modules/backOfficeModule.ts
+++ src/modules/backOfficeModule.ts
@@ -12,6 +12,7 @@ import { HomePage } from './../pages/home/home';
 import { ModuleBase } from "./moduelBase";
 import { SecurityGuard } from '../metadata/securityGuardModule';
 import { Customers } from '../pages/customers/customers';
+import { LogOut } from '../pages/logout/logout';
 
 @Injectable()
 @SecurityGuard(['BackOffice'])
@@ -30,7 +31,8 @@ export class BackOfficeModule implements ModuleBase {
     { title: 'Stores', icon: 'basket', component: Stores },
     { title: 'Price Books', icon: 'bookmark', component: PriceBooksPage },
     { title: 'Staffs Time Logs', icon: 'time', component: StaffsTimeLogs },
-    { title: 'Settings', icon: 'cog', component: Settings }
+    { title: 'Settings', icon: 'cog', component: Settings },
+    { title: 'Logout', icon: 'log-out', component: LogOut }
   ];    
 
   public pinTheMenu: boolean = true;
---
+++ src/pages/logout/logout.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { LoadingController, NavController } from "ionic-angular";
+import { Storage } from '@ionic/storage';
+import { LoginPage } from "../login/login";
+
+@Component({
+  selector: 'logout',
+  template: ``
+})
+export class LogOut {
+
+  constructor(
+    private loading: LoadingController,
+    private navCtrl: NavController,
+    private storage: Storage) {
+  }
+
+  async ionViewDidLoad() {
+    let loader = this.loading.create({
+      content: 'Logging Out...'
+    });
+    try {
+      await loader.present();
+      await this.storage.clear();
+      localStorage.clear();
+      // and finally remove pouchdb local data
+      loader.dismiss();
+      this.navCtrl.setRoot(LoginPage);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+}
