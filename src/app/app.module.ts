75038b5087001487163aebe75256a10877628d4f
rahil051 <infernus.rk31@gmail.com>
2017-12-11 02:35:53 +0500

    SC-63 1. Created network-monitor component 2. Added this component to some pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { Firebase } from '@ionic-native/firebase';
 import { Dialogs } from '@ionic-native/dialogs';
 import { Insomnia } from '@ionic-native/insomnia';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Network } from '@ionic-native/network';
 import { SharedModule } from './../modules/shared.module';
 
 // pages
@@ -123,6 +124,7 @@ import { SecurityService } from '../services/securityService';
 import { CustomerDetails } from '../pages/customer-details/customer-details';
 import { PlatformService } from '../services/platformService';
 import { AccountSettingService } from '../services/accountSettingService';
+import { NetworkMonitorModule } from '../components/network-monitor/network-monitor.module';
 
 const cloudSettings: CloudSettings = {
   'core': {
@@ -205,6 +207,7 @@ const cloudSettings: CloudSettings = {
 
     // custom
     SharedModule,
+    NetworkMonitorModule,
     TileItemsModule,
     BasketModule,
     PurchasableItemInfoModule,
@@ -270,6 +273,7 @@ const cloudSettings: CloudSettings = {
     { provide: ErrorHandler, useClass: AppErrorHandler },
     StatusBar,
     SplashScreen,
+    Network,
     Firebase,
     PinDialog,
     Dialogs,
---
+++ src/components/network-monitor/network-monitor.component.ts
@@ -0,0 +1,34 @@
+import { DBService } from './../../services/DBService';
+import { Network } from '@ionic-native/network';
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'network-monitor',
+  template: `<button ion-button icon-only class="bar-button bar-button-md bar-button-default bar-button-default-md">
+        <ion-icon [name]="networkIcon"></ion-icon>
+      </button>
+      <button *ngIf="syncing" ion-button icon-only class="bar-button bar-button-md bar-button-default bar-button-default-md">
+        <ion-icon name="sync"></ion-icon>
+      </button>`
+})
+export class NetworkMonitorComponent {
+  public networkIcon: string = 'eye';
+  public syncing: boolean = false;
+
+  constructor(private network: Network) {
+    this.network.onDisconnect().subscribe(() => this.networkIcon = "eye-off");
+    this.network.onConnect().subscribe(() => this.networkIcon = "eye");
+
+    DBService.criticalDBSyncProgress.subscribe(
+      async data => {
+        this.syncing = data === 1;
+      }
+    );
+
+    DBService.dbSyncProgress.subscribe(
+      async data => {
+        this.syncing = data === 1;
+      }
+    );
+  }
+}
