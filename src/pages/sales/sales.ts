bd65e862fdf1253a5f2019eeb2a60c0a5b4619ee
rahil051 <infernus.rk31@gmail.com>
2017-12-14 19:11:29 +0500

    SC-117 1. Improved item tiles loading in Sales screen to work with barcode scanner

--- src/pages/sales/sales.ts
+++ src/pages/sales/sales.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment';
 import { PurchasableItemPriceInterface } from './../../model/purchasableItemPrice.interface';
 import { EvaluationContext } from './../../services/EvaluationContext';
 import { Component, ChangeDetectorRef, ViewChild } from '@angular/core';
-import { NavController, LoadingController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, LoadingController, Loading, NavParams, ToastController } from 'ionic-angular';
 
 import { SharedService } from './../../services/_sharedService';
 import { SalesServices } from '../../services/salesService';
@@ -36,6 +36,10 @@ interface InteractableItem extends PurchasableItem {
   employeeId: string;
 }
 
+interface PurchasableItemTiles {
+  [id: string]: PurchasableItem[]
+}
+
 @PageModule(() => SalesModule)
 @Component({
   selector: 'page-variables',
@@ -49,6 +53,7 @@ export class Sales {
   private basketComponent: BasketComponent;
 
   public categories: any[];
+  public purchasableItems: PurchasableItemTiles = {};
   public activeCategory: any;
   public activeTiles: any[];
   public invoice: Sale;
@@ -130,7 +135,7 @@ export class Sales {
     if (_init) {
       let loader = this.loading.create({ content: 'Loading Register...', });
       await loader.present();
-      await this.initiate();
+      await this.initiate(loader);
       loader.dismiss();
     }
   }
@@ -142,10 +147,13 @@ export class Sales {
    */
   public selectCategory(category) {
     this.activeCategory = category;
+    this.activeTiles = this.purchasableItems[this.activeCategory._id]
+    /*
     this.salesService.loadPurchasableItems(category._id).then(
       items => this.activeTiles = _.sortBy(items, [item => parseInt(item.order) || 0]),
       error => { console.error(error); }
     );
+    */
     return category._id == category._id;
   }
 
@@ -182,7 +190,7 @@ export class Sales {
   public paymentClicked($event) {
     let pushCallback = params => {
       return new Promise((resolve, reject) => {
-        if(params) {
+        if (params) {
           this.salesService.instantiateInvoice().then(response => {
             this.invoiceParam = null;
             this.invoice = response.invoice;
@@ -224,18 +232,18 @@ export class Sales {
     this.priceBooks = data[3] as PriceBook[];
     this.priceBook = data[1] as PriceBook;
 
-        if(invoiceData.invoice.customerKey) {
-          // parallel
-					this.customerService.get(invoiceData.invoice.customerKey)
-						.then(customer => this.customer = customer)
-            .catch(err => {  })
-        }
+    if (invoiceData.invoice.customerKey) {
+      // parallel
+      this.customerService.get(invoiceData.invoice.customerKey)
+        .then(customer => this.customer = customer)
+        .catch(err => { })
+    }
 
-        this.priceBooks.sort(
-          firstBy("priority").thenBy((book1, book2) => {
-            return new Date(book2._id).getTime() - new Date(book1._id).getTime();
-          })
-        );
+    this.priceBooks.sort(
+      firstBy("priority").thenBy((book1, book2) => {
+        return new Date(book2._id).getTime() - new Date(book1._id).getTime();
+      })
+    );
 
     if (invoiceData.doRecalculate) {
       var _invoice: Sale = await this.salesService.reCalculateInMemoryInvoice(
@@ -252,36 +260,47 @@ export class Sales {
     };
   }
 
-  private async loadCategoriesAssociation(categories: any[]): Promise<any> {
-    let promises: Promise<any>[] = [];
-    for (let categoryIndex = categories.length - 1; categoryIndex >= 0; categoryIndex--) {
-      promises.push(new Promise(async (resolve, reject) => {
-        if (categoryIndex === 0) {
-          this.activeCategory = categories[categoryIndex];
-          var items: Array<any> = await this.salesService.loadPurchasableItems(categories[categoryIndex]._id)
-          this.activeTiles = _.sortBy(items, [item => parseInt(item.order) || 0]);
-        } else {
-          await this.salesService.loadPurchasableItems(categories[categoryIndex]._id);
-        }
-      }));
+  private async loadCategoriesAndAssociations(): Promise<any> {
+    try {
+      let categories = await this.categoryService.getAll();
+      let promises: any[] = [];
+      categories.forEach((category, index, catArray) => {
+        promises.push(async () => {
+          let items: any[] = await this.salesService.loadPurchasableItems(category._id);
+          if (items.length > 0) {
+            this.purchasableItems[category._id] = _.sortBy(items, [item => parseInt(item.order) || 0]);
+          } else {
+            catArray[index] = null;
+          }
+          return;
+        });
+      });
+      await Promise.all(promises.map(promise => promise()));
+      this.categories = _.sortBy(_.compact(categories), [category => parseInt(category.order) || 0]);
+      this.activeCategory = _.head(this.categories);
+      this.activeTiles = this.purchasableItems[this.activeCategory._id];
+      return;
+    } catch (err) {
+      return Promise.reject(err);
     }
-
-    return await Promise.all(promises);
   }
 
-  public async initiate(): Promise<any> {
+  public async initiate(loader: Loading): Promise<any> {
     this.cdr.detach();
     let promises: any[] = [
       async () => {
-        this.categories = await this.categoryService.getAll();
-        this.categories = _.sortBy(this.categories, [category => parseInt(category.order) || 0]);
-        this.loadCategoriesAssociation(this.categories);
+        loader.setContent('Loading Items...');
+        await this.loadCategoriesAndAssociations();
       },
-      async () => await this.initSalePageData()
+      async () => {
+        loader.setContent('Loading Sales Data...');
+        await this.initSalePageData();
+      }
     ];
 
     if (this.user.settings.trackEmployeeSales) {
       promises.push(async () => {
+        loader.setContent('Loading Employees...');
         this.employees = await this.employeeService.getListByCurrentStatus();
         if (this.employees && this.employees.length > 0) {
           this.employees = this.employees.map(employee => {
@@ -294,6 +313,7 @@ export class Sales {
     try {
       await Promise.all(promises.map(func => func()));
       this.cdr.reattach();
+
       return;
     } catch (err) {
       return Promise.reject(err);
@@ -304,7 +324,7 @@ export class Sales {
     let loader = this.loading.create({ content: 'Opening Register...' });
     try {
       await loader.present();
-      await this.initiate();
+      await this.initiate(loader);
       this.register.openTime = moment().utc().format();
       this.register.status = true;
       this.register.openingAmount = Number(this.register.openingAmount);
