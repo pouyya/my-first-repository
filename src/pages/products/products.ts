e67a63d853d0ab1e8e9ed34a3b1fea16c8a46106
rahil051 <infernus.rk31@gmail.com>
2017-12-26 17:00:10 +0500

    SC-10 SC-13 1. Updated Products listings page with new columns and data

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,47 +1,115 @@
+import { StoreService } from './../../services/storeService';
+import { PriceBookService } from './../../services/priceBookService';
+import { PriceBook } from './../../model/priceBook';
+import _ from 'lodash';
+import { LoadingController } from 'ionic-angular';
+import { StockHistoryService } from './../../services/stockHistoryService';
 import { Component, NgZone } from '@angular/core';
 import { NavController, AlertController, Platform } from 'ionic-angular';
 import { ProductService } from '../../services/productService';
 import { ProductDetails } from '../product-details/product-details';
 import { InventoryModule } from '../../modules/inventoryModule';
 import { PageModule } from '../../metadata/pageModule';
+import { Product } from '../../model/product';
+
+interface ProductsList extends Product {
+  stockInHand: number; /** Stock of all shops */
+  retailPrice: number /** From default pricebook */
+}
 
 @PageModule(() => InventoryModule)
 @Component({
-  templateUrl: 'products.html'
+  templateUrl: 'products.html',
+  styleUrls: ['/pages/products/products.scss']
 })
 export class Products {
-  public items = [];
-  public itemsBackup = [];
+  public items: ProductsList[] = [];
+  public itemsBackup: ProductsList[] = [];
+  private storeIds: string[];
+  private priceBook: PriceBook;
+  private readonly defaultLimit = 10;
+  private readonly defaultOffset = 0;
+  private limit: number;
+  private offset: number;
+  private total: number;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    private navCtrl: NavController,
     private alertCtrl: AlertController,
     private service: ProductService,
+    private stockHistoryService: StockHistoryService,
+    private priceBookService: PriceBookService,
+    private storeService: StoreService,
     private platform: Platform,
+    private loading: LoadingController,
     private zone: NgZone) {
   }
 
-  ionViewDidEnter() {
-    this.platform.ready().then(() => {
+  async ionViewDidEnter() {
+    try {
+      let loader = this.loading.create({ content: 'Loading Products...' });
+      await loader.present();
+      this.priceBook = await this.priceBookService.getDefault();
+      this.storeIds = (await this.storeService.getAll()).map(store => store._id);
+      let products: ProductsList[] = await this.loadProducts();
+      await this.platform.ready();
+      this.zone.run(() => {
+        this.items = products;
+        this.itemsBackup = products;
+        console.warn(this.items)
+        loader.dismiss();
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 
-      this.service.getAll()
-        .then(data => {
-          this.zone.run(() => {
-            this.items = data;
-            this.itemsBackup = data;
+  showDetail(product: ProductsList) {
+    this.navCtrl.push(ProductDetails, { item: <Product>_.omit(product, ['stockInHand']) });
+  }
+
+  async delete(product: ProductsList, index) {
+    await this.service.delete(<Product>_.omit(product, ['stockInHand']));
+    this.items.splice(index, 1);
+  }
+
+  public async loadProducts(): Promise<ProductsList[]> {
+    try {
+      let products: any[] = await this.service.getAll();
+      if (products) {
+        let fetchStockInHand: any[] = [];
+        products.forEach((product, index, array) => {
+          fetchStockInHand.push(async () => {
+            let storeStock: any[] = await this.stockHistoryService
+              .collectProductTotalStockValueForEachStore(product._id, this.storeIds);
+            array[index].stockInHand = storeStock.length <= 0 ? 0 : storeStock
+              .map(stock => stock.totalValue)
+              .reduce((a, b) => a + b);
+            let itemIdx: number = _.findIndex(this.priceBook.purchasableItems, { id: product.id });
+            if (itemIdx > -1) {
+              array[index].retailPrice = this.priceBook.purchasableItems[itemIdx].retailPrice;
+              this.priceBook.purchasableItems.splice(itemIdx, 1);
+            } else {
+              array[index].retailPrice = 0;
+            }
+            // let purchasableItem = _.find(this.priceBook.purchasableItems, { id: product.id });
+            // array[index].retailPrice = purchasableItem === undefined ? 0 : purchasableItem.retailPrice;
+            return;
           });
-        })
-        .catch(console.error.bind(console));
-    });
+        });
+
+        await Promise.all(fetchStockInHand.map(fn => fn()));
+        return <ProductsList[]>products;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
-  showDetail(item) {
-    this.navCtrl.push(ProductDetails, { item: item });
-  }
+  public loadMore() {
 
-  delete(item, idx) {
-    this.service.delete(item)
-      .catch(console.error.bind(console));
-    this.items.splice(idx, 1);
   }
 
   getItems(event) {
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,11 +1,40 @@
+import _ from 'lodash';
 import { Injectable } from '@angular/core';
-import {Product } from '../model/product';
-import {BaseEntityService} from  './baseEntityService';
+import { Product } from '../model/product';
+import { BaseEntityService } from './baseEntityService';
 
 @Injectable()
-export class ProductService  extends BaseEntityService<Product> {  
-    constructor() 
-    {
-        super(Product);
-    }
+export class ProductService extends BaseEntityService<Product> {
+	constructor() {
+		super(Product);
+	}
+
+	public async searchProducts(limit: number = 10, offset: number = 0, options?: any) {
+		var query: any = {
+			selector: {}
+		};
+		_.each(options, (value, key) => {
+			if (value) {
+				query.selector[key] = _.isArray(value) ? { $in: value } : value;
+			}
+		});
+		let countQuery = _.cloneDeep(query);
+		query.limit = limit;
+		query.offset = offset;
+
+		let promises: any[] = [
+			async () => {
+				let data = await this.findBy(countQuery);
+				return data.length;
+			},
+			async () => await this.findBy(query)
+		];
+
+		try {
+			let [totalCount, docs] = await Promise.all(promises.map(p => p()));
+			return { totalCount, docs };
+		} catch (err) {
+			return Promise.reject(err);
+		}
+	}
 }
