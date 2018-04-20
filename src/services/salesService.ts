5fb7d5e9658b9205b0692d3ad1105c5923debed5
Zohaib Shakir <zohaibshakir2010@gmail.com>
2018-04-20 13:00:11 +0500

    SC-197 Fixed the issue for Selecting all failing in SalesHistory

--- src/services/salesService.ts
+++ src/services/salesService.ts
@@ -125,7 +125,7 @@ export class SalesServices extends BaseEntityService<Sale> {
 
 		if (options) {
 			_.each(options, (value, key) => {
-				if (value !== undefined) {
+				if (value != undefined) {
 					query.selector.$and.push({ [key]: _.isArray(value) ? { $in: value } : value });
 				}
 			});
