21107f23b702fc61415a4b9e28a8cc4355bc4d6d
Aria Zanganeh <admin@simpleidea.com.au>
2018-04-30 08:19:50 +1000

    SC-200 code cleanup

--- src/metadata/validationModule.ts
+++ src/metadata/validationModule.ts
@@ -1,4 +1,4 @@
-import {Validators} from "@angular/forms";
+import { Validators } from "@angular/forms";
 
 export enum ValidationOptions {
     Required = "Required",
@@ -6,8 +6,8 @@ export enum ValidationOptions {
 }
 
 const validationOptionsMapping = {
-  "Required": Validators.required,
-  "Regex": Validators.pattern,
+    "Required": Validators.required,
+    "Regex": Validators.pattern,
 }
 
 export class ValidationMeta {
@@ -21,64 +21,66 @@ export class Validation {
     public value: string;
 }
 
-export class ValidationInfo{
-    public static validationsList: Array<ValidationMeta> = new Array<ValidationMeta> ();
+export class ValidationInfo {
+    public static validationsList: Array<ValidationMeta> = new Array<ValidationMeta>();
 
-    public static getValidationsMeta(entityTypeName){
+    public static getValidationsMeta(entityTypeName) {
         const validations = ValidationInfo.validationsList.filter(validation => validation.entityTypeName === entityTypeName);
-        if(validations.length){
-          return validations[0];
+        if (validations.length) {
+            return validations[0];
         }
         return null;
     }
 
-    public static getAllValidations(entityTypeName){
-      const validationMeta = ValidationInfo.getValidationsMeta(entityTypeName);
-      if(!validationMeta){
-        return null;
-      }
-      const validationsMapping = validationMeta.validations.reduce((initialObj, validation) => {
-          if(!initialObj[validation.variableName]){
-            initialObj[validation.variableName] = [];
-          }
+    public static getAllValidations(entityTypeName) {
+        const validationMeta = ValidationInfo.getValidationsMeta(entityTypeName);
 
-          if(validation.type == ValidationOptions.Required){
-              initialObj[validation.variableName].push(validationOptionsMapping[validation.type]);
-          }else{
-              initialObj[validation.variableName].push(validationOptionsMapping[validation.type](validation.value));
-          }
+        if (!validationMeta) {
+            return null;
+        }
 
-          return initialObj;
-      }, {});
+        const validationsMapping = validationMeta.validations.reduce((initialObj, validation) => {
+            if (!initialObj[validation.variableName]) {
+                initialObj[validation.variableName] = [];
+            }
 
-      return validationsMapping;
+            if (validation.type == ValidationOptions.Required) {
+                initialObj[validation.variableName].push(validationOptionsMapping[validation.type]);
+            } else {
+                initialObj[validation.variableName].push(validationOptionsMapping[validation.type](validation.value));
+            }
+
+            return initialObj;
+        }, {});
+
+        return validationsMapping;
     }
 }
 
 // Decorators
 export function Required() {
-  return function Required(target: any, propertyKey: string){
-      const entityTypeName = target.constructor.name;
-      let validationsMeta = ValidationInfo.getValidationsMeta(entityTypeName);
-      if(!validationsMeta){
-        validationsMeta = new ValidationMeta();
-        validationsMeta.entityTypeName = entityTypeName;
-        validationsMeta.validations = [];
-        ValidationInfo.validationsList.push(validationsMeta);
-      }
+    return function Required(target: any, propertyKey: string) {
+        const entityTypeName = target.constructor.name;
+        let validationsMeta = ValidationInfo.getValidationsMeta(entityTypeName);
+        if (!validationsMeta) {
+            validationsMeta = new ValidationMeta();
+            validationsMeta.entityTypeName = entityTypeName;
+            validationsMeta.validations = [];
+            ValidationInfo.validationsList.push(validationsMeta);
+        }
 
-      let validation = new Validation();
-      validation.variableName = propertyKey;
-      validation.type = ValidationOptions.Required;
-      validationsMeta.validations.push(validation);
-  }
+        let validation = new Validation();
+        validation.variableName = propertyKey;
+        validation.type = ValidationOptions.Required;
+        validationsMeta.validations.push(validation);
+    }
 };
 
 export function Regex(regex: string) {
-    return function Required(target: any, propertyKey: string){
+    return function Required(target: any, propertyKey: string) {
         const entityTypeName = target.constructor.name;
         let validationsMeta = ValidationInfo.getValidationsMeta(entityTypeName);
-        if(!validationsMeta){
+        if (!validationsMeta) {
             validationsMeta = new ValidationMeta();
             validationsMeta.entityTypeName = entityTypeName;
             validationsMeta.validations = [];
