c18f1e35a307f0d2ffee5d9f868d6b3bda120c07
saber tabatabaee <saber.tabatabaee@gmail.com>
2018-05-04 03:20:22 +0430

    SC-210 only run gulp - with gulp we can gather all txt in all txt in view folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
- 
-gulp.task('scripts', function() {
-  return gulp.src('src/data/views/*.txt')
-    .pipe(concat('all.txt'))
-    .pipe(gulp.dest('src/data/'));
+
+gulp.task('scripts', function () {
+    return gulp.src('src/data/views/**/*.txt')
+        .pipe(concat('all.txt'))
+        .pipe(gulp.dest('src/data/views/'));
 });
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     gulp.run('scripts');
-
-    gulp.watch('app/src/**', function(event) {
-        gulp.run('scripts');
-    })
-
 })
