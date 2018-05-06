ed5b34036ef6d6b026e8c5b6bcf80150fd83685b
Aria Zanganeh <admin@simpleidea.com.au>
2018-05-06 16:58:23 +1000

    SC-210 resolve newline - comma separated

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 gulp.task('scripts', function () {
-    return gulp.src('src/data/views/**/*.txt')
-        .pipe(concat('all.txt'))
+    return gulp.src(['src/data/views/**/*.txt', '!src/data/views/all.txt'])
+        .pipe(concat('all.txt', {newLine: ','}))
         .pipe(gulp.dest('src/data/views/'));
 });
 
