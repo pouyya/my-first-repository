7e3b5f6a5526f936a3a7efcab065da81d0ee68cc
saber tabatabaee <saber.tabatabaee@gmail.com>
2018-06-07 19:49:02 +0430

    SC-260 as SC-217

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var fs = require('fs');
+var path = require('path');
+
+var scriptsPath = 'src/data';
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
 
 gulp.task('scripts', function () {
-    return gulp.src(['src/data/views/**/*.txt', '!src/data/views/all.txt'])
-        .pipe(concat('all.txt', {newLine: ','}))
-        .pipe(gulp.dest('src/data/views/'));
+    var folders = getFolders(scriptsPath);
+    if (folders.length === 0) return done(); // nothing to do!
+ 
+    var tasks = folders.map(function(folder) {
+        return gulp.src(['src/data/'+folder+'/views/**/*.txt', '!src/data/'+folder+'/views/all.txt'])
+            .pipe(concat('all.txt', {newLine: ','}))
+            .pipe(gulp.dest('src/data/'+folder+'/views/'));
+    });
 });
 
 gulp.task('default', function () {
