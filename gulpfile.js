4d5a01ae570da7d3f6217ce6f6aeb7bfb45dba39
saber tabatabaee <saber.tabatabaee@gmail.com>
2018-06-12 21:31:12 +0430

    SC-260

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,41 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var fs = require('fs');
-var path = require('path');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var fs = require("fs");
+var path = require("path");
+ var filter = require('gulp-filter');
+const size = require('gulp-size');
 
-var scriptsPath = 'src/data';
+var scriptsPath = "src/data/";
 
 function getFolders(dir) {
-    return fs.readdirSync(dir)
-      .filter(function(file) {
-        return fs.statSync(path.join(dir, file)).isDirectory();
-      });
+  return fs.readdirSync(dir).filter(function(file) {
+    return fs.statSync(path.join(dir, file)).isDirectory();
+  });
 }
 
-gulp.task('scripts', function () {
-    var folders = getFolders(scriptsPath);
-    if (folders.length === 0) return done(); // nothing to do!
- 
-    var tasks = folders.map(function(folder) {
-        return gulp.src(['src/data/'+folder+'/views/**/*.txt', '!src/data/'+folder+'/views/all.txt'])
-            .pipe(concat('all.txt', {newLine: ','}))
-            .pipe(gulp.dest('src/data/'+folder+'/views/'));
+var business = {
+  types: ["barber", "coffee-shop", "health-and-beauty", "other"]
+};
+
+gulp.task("scripts", function() {
+  var folders = getFolders(scriptsPath);
+  if (folders.length === 0) return done(); // nothing to do!
+
+  var tasks = folders.map(function(folder) {
+    business.types.map(function(type) {
+      return gulp
+        .src([
+          scriptsPath + folder + "/business-data/" + type + "-data.json",
+          scriptsPath + folder + "/views/*.json",
+          scriptsPath + folder + "/base-data.json",
+          //"!src/data/temp/**"
+        ])
+        .pipe(concat(folder + ".json",  { newLine: "," }))
+        .pipe(gulp.dest(scriptsPath + "temp/" + type + "/"));
     });
+  });
 });
 
-gulp.task('default', function () {
-    gulp.run('scripts');
-})
+gulp.task("default", function() {
+  gulp.run("scripts");
+});
