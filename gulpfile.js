a22c9e137573f67aea1a95148161e5d951d61a0b
Aria Zanganeh <admin@simpleidea.com.au>
2018-06-14 17:30:32 +1000

    SC-260 resovle issue of adding extra comma to file

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,68 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 var fs = require("fs");
 var path = require("path");
+var through = require('through2');
 
 var scriptsPath = "src/data/";
 
 function getFolders(dir) {
-  return fs.readdirSync(dir).filter(function(file) {
+  return fs.readdirSync(dir).filter(function (file) {
     return fs.statSync(path.join(dir, file)).isDirectory();
   });
 }
+
+function clip() {
+  return through.obj(function (file, enc, callback) {
+
+    if (!file.isNull() && file.contents.toString().trim().length > 0) {
+      this.push(file);
+      return callback();
+    }
+
+    callback();
+  });
+};
 
 var business = {
   types: ["barber", "coffee-shop", "health-and-beauty", "other"]
 };
 
-gulp.task("scripts", function() {
-  var folders = getFolders(scriptsPath);
-  if (folders.length === 0) return done();
+gulp.task("build-scripts", function () {
 
-  var tasks = folders.map(function(folder) {
-    business.types.map(function(type) {
+  var argv = require('yargs').argv;
+
+  if (!argv.dest) {
+    console.log('Please provide destination folder using --dest');
+    return;
+  }
+
+  var folders = getFolders(scriptsPath);
+  if (folders.length === 0) return;
+
+  if (!fs.existsSync(argv.dest)) {
+    fs.mkdir(argv.dest);
+  }
+
+  return folders.map(function (folder) {
+
+    return business.types.map(function (type) {
+
+      if (!fs.existsSync(argv.dest + type)) {
+        fs.mkdirSync(argv.dest + type);
+      }
+
+      fs.writeFileSync(argv.dest + type + "/" + folder + ".json", "");
       return gulp
         .src([
           scriptsPath + folder + "/business-data/" + type + "-data.json",
           scriptsPath + folder + "/views/*.json",
           scriptsPath + folder + "/base-data.json"
         ])
+        .pipe(clip())
         .pipe(concat(folder + ".json", { newLine: "," }))
-        .pipe(gulp.dest("../spos-plugin/simple-idea-couchdb-integration/json/" + type + "/"))
-        
+        .pipe(gulp.dest(argv.dest + type + "/"))
     });
   });
 });
 
-
-
-
-gulp.task("default", function() {
-    gulp.run("scripts");
-});
+gulp.task("default", ["build-scripts"]);
