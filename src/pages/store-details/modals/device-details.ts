41890cfa124b25b9061a5de8307b362a4c6bd980
saber tabatabaee yazdi <sabertabatabaee@gmail.com>
2018-07-17 11:05:31 +0430

    revert back all

--- src/pages/store-details/modals/device-details.ts
+++ src/pages/store-details/modals/device-details.ts
@@ -1,6 +1,6 @@
 import {
   NavParams, AlertController, LoadingController,
-  ViewController, ToastController
+  ViewController
 } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { SyncContext } from "../../../services/SyncContext";
@@ -38,8 +38,7 @@ export class DeviceDetailsModal {
     private serviceService: ServiceService,
     private alertCtrl: AlertController,
     private syncContext: SyncContext,
-    private loading: LoadingController,
-    private toastController: ToastController,
+    private loading: LoadingController
   ) { }
 
   async ionViewDidLoad() {
@@ -66,7 +65,6 @@ export class DeviceDetailsModal {
       })
     }
     loader.dismiss();
-    this.validate(0);
   }
 
   public dismiss() {
@@ -95,47 +93,5 @@ export class DeviceDetailsModal {
     });
 
     confirm.present();
-  }
-
-  validate(flagToast) {
-    this.changeColorAndValidation();
-
-    if (flagToast == 1 && this.IpColor == "red") {
-      let toast = this.toastController.create({
-        message: "Missing any set of digits? for printer IP Address",
-        duration: 3000
-      });
-      toast.present();
-    }
-
-    if (flagToast == 1 && this.PortColor == "red") {
-      let toast = this.toastController.create({
-        message: "Please check your printer port number",
-        duration: 3000
-      });
-      toast.present();
-    }
-  }
-
-  validateIPaddress() {
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(this.device.ipAddress)) {
-      return (true)
-    }
-    return (false)
-  }
-
-  validatePortNumber() {
-    if (/^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/.test(this.device.printerPort)) {
-      return (true)
-    }
-    return (false)
-  }
-
-  changeColorAndValidation() {
-    let vIpAddress = this.validateIPaddress();
-    let vPortNumber = this.validatePortNumber();
-    this.formValidation = (vIpAddress && vPortNumber) ? true : false;
-    this.IpColor = (vIpAddress ? "green" : "red");
-    this.PortColor = (vPortNumber ? "green" : "red");
   }
 }
