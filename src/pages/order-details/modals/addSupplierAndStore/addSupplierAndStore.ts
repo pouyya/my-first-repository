c7cccc9b9cca6062846a00ae71f40463d1e30786
saber tabatabaee yazdi <sabertabatabaee@gmail.com>
2018-07-19 16:16:35 +0430

    you can use lodash

--- src/pages/order-details/modals/addSupplierAndStore/addSupplierAndStore.ts
+++ src/pages/order-details/modals/addSupplierAndStore/addSupplierAndStore.ts
@@ -8,59 +8,55 @@ import { CreateSupplier } from '../createSupplier/createSupplier';
 import { SyncContext } from '../../../../services/SyncContext';
 
 @Component({
-    selector: 'add-supplier-and-store',
-    templateUrl: 'add-supplier-and-store.html'
+	selector: 'add-supplier-and-store',
+	templateUrl: 'add-supplier-and-store.html'
 })
 export class AddSupplierAndStore {
+	public suppliers: Supplier[] = [];
+	public stores: Store[] = [];
 
-    public suppliers: Supplier[] = [];
-    public stores: Store[] = [];
+	public selectedSupplier: Supplier = null;
+	public selectedStore: Store = null;
 
-    public selectedSupplier: Supplier = null;
-    public selectedStore: Store = null;
+	private navPopCallback: any;
 
-    private navPopCallback: any;
+	constructor(
+		private supplierService: SupplierService,
+		private storeService: StoreService,
+		private navParams: NavParams,
+		private modalCtrl: ModalController,
+		private syncContext: SyncContext
+	) {
+		this.navPopCallback = this.navParams.get('callback');
+	}
 
-    constructor(
-        private supplierService: SupplierService,
-        private storeService: StoreService,
-        private navParams: NavParams,
-        private modalCtrl: ModalController,
-        private syncContext: SyncContext
-    ) {
-        this.navPopCallback = this.navParams.get('callback');
-    }
+	async ionViewDidLoad() {
+		[ this.suppliers, this.stores ] = [ await this.supplierService.getAll(), await this.storeService.getAll() ];
 
-    async ionViewDidLoad() {
-        [this.suppliers, this.stores] = [
-            await this.supplierService.getAll(),
-            await this.storeService.getAll()
-        ];
+		let storeId = this.syncContext.currentStore && this.syncContext.currentStore._id;
+		if (!storeId) {
+			this.selectedStore = this.stores[0];
+			storeId = this.selectedStore._id;
+		} else {
+			this.selectedStore = _.find(this.stores, { _id: storeId });
+		}
+	}
 
-        let storeId = this.syncContext.currentStore && this.syncContext.currentStore._id;
-        if (!storeId) {
-            this.selectedStore = this.stores[0];
-            storeId = this.selectedStore._id;
-        } else {
-            this.selectedStore = _.find(this.stores, { _id: storeId });
-        }
-    }
+	ionViewDidLeave() {
+		this.navPopCallback({
+			supplier: this.selectedSupplier,
+			store: this.selectedStore
+		});
+	}
 
-    ionViewDidLeave() {
-        this.navPopCallback({
-            supplier: this.selectedSupplier,
-            store: this.selectedStore
-        });
-    }
-
-    public addSupplier() {
-        let modal = this.modalCtrl.create(CreateSupplier);
-        modal.onDidDismiss((supplier: Supplier) => {
-            if (supplier) {
-                this.selectedSupplier = supplier;
-                this.suppliers.push(supplier);
-            }
-        });
-        modal.present();
-    }
+	public addSupplier() {
+		let modal = this.modalCtrl.create(CreateSupplier);
+		modal.onDidDismiss((supplier: Supplier) => {
+			if (supplier) {
+				this.selectedSupplier = supplier;
+				this.suppliers.push(supplier);
+			}
+		});
+		modal.present();
+	}
 }
