fc7f8ff74ca1d0c6cd2cf628cdb3561b8161d8f2
saber tabatabaee yazdi <sabertabatabaee@gmail.com>
2018-08-20 21:12:48 +0430

    17

---
+++ src/components/network-monitor/network-monitor-report.component.ts
@@ -0,0 +1,56 @@
+import { DBEvent } from '@simplepos/core/dist/db/dbEvent';
+import { DBService } from '@simplepos/core/dist/services/dBService';
+import { Network } from '@ionic-native/network';
+import { Component } from "@angular/core";
+
+@Component({
+  selector: '[network-monitor-report]',
+  template: `<button class="no-cursor bar-button bar-button-md bar-button-default bar-button-default-md" ion-button icon-only clear >
+        <ion-icon [name]="networkIcon"></ion-icon>
+      </button>
+      <button class="no-cursor bar-button bar-button-md bar-button-default bar-button-default-md" ion-button icon-only clear >
+          <ion-icon [name]="syncIcon"></ion-icon>
+        </button>`,
+  styles: [
+    `button > ion-spinner * {
+      width: 28px;
+      height: 28px;
+      stroke: white;
+      fill: white;
+    }
+    .no-cursor {
+        pointer-events: none;
+        cursor: default;
+    }
+    `
+  ]
+})
+export class NetworkMonitorReportComponent {
+
+  public networkIcon: string = 'eye';
+  public syncIcon: string = 'cloud-outline';
+  public networkState: boolean = false;
+
+  constructor(private network: Network) {
+    this.network.onDisconnect().subscribe(() => {
+      this.networkIcon = "eye-off";
+      this.networkState = false;
+    });
+    this.network.onConnect().subscribe(() => {
+      this.networkIcon = "eye";
+      this.networkState = true;
+    });
+
+    DBService.pouchDBProvider.criticalDBSyncProgress.subscribe(
+      (data: DBEvent) => {
+        data && (this.syncIcon = data.isActive ? 'cloud-upload' : 'cloud-outline');
+      }
+    );
+
+    DBService.pouchDBProvider.dbSyncProgress.subscribe(
+      (data: DBEvent) => {
+        data && (this.syncIcon = data.isActive ? 'cloud-upload' : 'cloud-outline');
+      }
+    );
+  }
+}
