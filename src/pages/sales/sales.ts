f113931753083be2cacbbf855ca827d9134491b2
ghobakhloum <ghobakhloum@gmail.com>
2018-10-25 01:13:45 +0330

    add default value

--- src/pages/sales/sales.ts
+++ src/pages/sales/sales.ts
@@ -68,7 +68,7 @@ export class Sales implements OnDestroy {
   public employees: any[] = [];
   public selectedEmployee: Employee = null;
   public user: UserSession;
-  private isTableEnabled: boolean;
+  private isTableEnabled: boolean = false;
   private alive: boolean = true;
   public iconTakeaway: string = "person-add";
 
