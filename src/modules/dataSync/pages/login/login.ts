808ad78c0869f4f04485bc913d1083bc539cfe4d
Aria Zanganeh <zanganeh@gmail.com>
2018-10-29 16:47:25 +1100

    578 - add support implicit login flow for the app

--- src/modules/dataSync/pages/login/login.ts
+++ src/modules/dataSync/pages/login/login.ts
@@ -1,6 +1,4 @@
-import { ModalController, NavController } from 'ionic-angular';
-import { ToastController } from 'ionic-angular';
-import { LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/authService';
 import { DataSync } from '../dataSync/dataSync';
@@ -17,25 +15,17 @@ import { ConfigService } from '../../../../modules/dataSync/services/configServi
 })
 export class LoginPage {
 
-  public email: string;
-  public password: string;
-
   constructor(
     private loading: LoadingController,
     public navCtrl: NavController,
     private authService: AuthService,
     private toastCtrl: ToastController,
-    private modalCtrl: ModalController,
     private iab: InAppBrowser,
     private userService: UserService) {
   }
 
   public async login(): Promise<any> {
 
-    this.userLogin(this.email, this.password);
-  }
-
-  async userLogin(email: string, password: string) {
     let loader = this.loading.create({
       content: 'Logging In...'
     });
@@ -43,7 +33,13 @@ export class LoginPage {
     await loader.present();
 
     try {
-      await this.authService.login(email, password);
+      const success = await this.idsLogin();
+      const keyValuePair = `#id_token=${encodeURIComponent(success.id_token)}&access_token=${encodeURIComponent(success.access_token)}`;
+      await this.authService.tryLogin({
+        customHashFragment: keyValuePair,
+        disableOAuth2StateCheck: true
+      });
+      await this.userService.loadUserProfile();
       if (this.userService.ensureRequiredClaims()) {
         await this.userService.initializeUserProfile();
         await this.navigateToDataSync();
@@ -51,12 +47,59 @@ export class LoginPage {
         this.authService.logout();
         this.showMessage("The minimum requirement is not available for your account. Please contact support.");
       }
-    } catch (error) {
+    }
+    catch (error) {
       var message = (error && error.status === 0) ? 'There is no internet connection pleas check your internet connection!' : 'Invalid Email/Password!';
       this.showMessage(message);
-    } finally {
+    }
+    finally {
       loader.dismiss();
     }
+  }
+
+  idsLogin(): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+
+      return this.authService.createAndSaveNonce().then(nonce => {
+        let state: string = Math.floor(Math.random() * 1000000000).toString();
+        if (window.crypto) {
+          const array = new Uint32Array(1);
+          window.crypto.getRandomValues(array);
+          state = array.join().toString();
+        }
+
+        this.authService.buildOAuthUrl(state, nonce).then((oauthUrl) => {
+
+          const browser = this.iab.create(oauthUrl, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,useWideViewPort=yes');
+
+          browser.on('loadstart').subscribe((event) => {
+            if ((event.url).indexOf('http://localhost:8100') === 0) {
+              browser.on('exit').subscribe(() => { });
+              browser.close();
+              const responseParameters = ((event.url).split('#')[1]).split('&');
+              const parsedResponse = {};
+              for (let i = 0; i < responseParameters.length; i++) {
+                parsedResponse[responseParameters[i].split('=')[0]] =
+                  responseParameters[i].split('=')[1];
+              }
+              const defaultError = 'Problem authenticating with SimplePOS IDS';
+              if (parsedResponse['state'] !== state) {
+                reject(defaultError);
+              } else if (parsedResponse['access_token'] !== undefined &&
+                parsedResponse['access_token'] !== null) {
+                resolve(parsedResponse);
+              } else {
+                reject(defaultError);
+              }
+            }
+          });
+          browser.on('exit').subscribe(function (event) {
+            reject('The SimplePOS IDS sign in flow was canceled');
+          });
+        });
+      });
+    });
   }
 
   private showMessage(message: string) {
@@ -75,41 +118,6 @@ export class LoginPage {
     return null;
   }
 
-  public register(): void {
-    const browser = this.iab.create(`${ConfigService.securityServerBaseUrl()}/register?ismobile=true`, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,useWideViewPort=yes');
-    var email;
-    var password;
-    var bridgeInterval;
-
-    var _this = this;
-
-    browser.on('loadstop').subscribe(function () {
-      bridgeInterval = setInterval(async function () {
-        var isRegistered = await _this.getValue(browser, 'isRegistered');
-        if (isRegistered) {
-
-          email = await _this.getValue(browser, 'email');
-          password = await _this.getValue(browser, 'password');
-
-          browser.close();
-        }
-
-        var closeResult = await _this.getValue(browser, 'forcetoclose');
-        if (closeResult && closeResult[0]) {
-          browser.close();
-        }
-      }, 500);
-
-    });
-
-    browser.on('exit').subscribe(function () {
-      clearInterval(bridgeInterval);
-      if (email && password) {
-        _this.userLogin(email, password);
-      }
-    });
-  }
-
   public forgotPassword(): void {
     const browser = this.iab.create(`${ConfigService.securityServerBaseUrl()}/forgottenpassword?ismobile=true`, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,useWideViewPort=yes');
     var bridgeInterval;
@@ -123,7 +131,7 @@ export class LoginPage {
         if (closeResult && closeResult[0]) {
           setTimeout(function () {
             browser.close();
-          }, 8000);          
+          }, 8000);
         }
       }, 500);
 
